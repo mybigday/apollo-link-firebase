d62b73d92f00b1eca08f3f1c7f920093c2ad7271
jhen <developer@jhen.me>
2019-09-27 15:54:09 +0800

    Update examples

--- examples/simple-todo/src/containers/TodoListContainer.tsx
+++ examples/simple-todo/src/containers/TodoListContainer.tsx
@@ -1,56 +1,61 @@
-import gql from 'graphql-tag';
-import {graphql, compose} from 'react-apollo';
-import TodoList from '../components/TodoList';
-import * as reject from 'lodash/reject';
-import { GET_TODO, REMOVE_TODO } from '../graphql';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+import TodoList from '../components/TodoList'
+
+import * as compose from 'lodash/flowright'
+import * as reject from 'lodash/reject'
+import { GET_TODO, REMOVE_TODO } from '../graphql'
 
 const removeTodo = graphql<any, any, any, any>(REMOVE_TODO, {
-  props: ({mutate}) => ({
-    removeTodo: ({userId, todoId}: {userId: string, todoId: string}) => {
+  props: ({ mutate }) => ({
+    removeTodo: ({ userId, todoId }: { userId: string; todoId: string }) => {
       return mutate({
-        variables: {todoRef: `/todos/${userId}/${todoId}`},
-        update: (proxy, {data: {addTodo}}) => {
+        variables: { todoRef: `/todos/${userId}/${todoId}` },
+        update: (proxy, { data: { addTodo } }) => {
           // Read the data from our cache for this query.
-          const data: {todos: any[]} = proxy.readQuery({
+          const data: { todos: any[] } = proxy.readQuery({
             query: GET_TODO,
             variables: {
-              todoRef: `/todos/${userId}`
-            }
-          });
+              todoRef: `/todos/${userId}`,
+            },
+          })
 
           // Add our todo from the mutation to the end.
-          const todos = reject(data.todos, o => o.id === todoId);
+          const todos = reject(data.todos, o => o.id === todoId)
           // Write our data back to the cache.
           proxy.writeQuery({
             query: GET_TODO,
             variables: {
-              todoRef: `/todos/${userId}`
+              todoRef: `/todos/${userId}`,
             },
             data: {
-              todos
-            }
-          });
-        }
-      });
-    }
-  })
-});
+              todos,
+            },
+          })
+        },
+      })
+    },
+  }),
+})
 
 const withTodos = graphql<any, any, any, any>(GET_TODO, {
-  props: ({data}) => {
+  props: ({ data }) => {
     if (data.loading || data.error) {
-      return {todos: []};
+      return { todos: [] }
     }
 
     return {
-      todos: data.todos
-    };
+      todos: data.todos,
+    }
   },
   options: props => {
     return {
-      variables: {todoRef: `/todos/${props.userId}`}
-    };
-  }
-});
+      variables: { todoRef: `/todos/${props.userId}` },
+    }
+  },
+})
 
-export default compose(withTodos, removeTodo)(TodoList);
+export default compose(
+  withTodos,
+  removeTodo,
+)(TodoList)
